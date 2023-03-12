00fa8019296a7366a4c8f598db5215b8df7bb40b
Collin Sinclair <collinsinclair88@gmail.com>
2023-03-11 17:24:16 -0700

    stars background update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,6 +26,15 @@
 </script>
 
 <body class="bg-zinc-900 min-h-screen">
+	<!--<body>-->
+	<div class="stars-container">
+		<div class="stars" />
+		<div class="stars" />
+		<div class="stars" />
+		<div class="stars" />
+		<div class="stars" />
+		<div class="stars" />
+	</div>
 	<nav>
 		<div class="px-3 py-3 flex items-center flex justify-between">
 			<button
@@ -83,3 +92,109 @@
 	</nav>
 	<slot />
 </body>
+
+<style lang="scss">
+	.stars-container,
+	.stars {
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		overflow: hidden;
+	}
+
+	.stars-container {
+		background-color: rgba(0,0,0,0)
+	}
+
+	.stars {
+		background-image: radial-gradient(
+				2px 2px at 37px 138px,
+				rgb(208, 208, 208),
+				rgb(152, 163, 188),
+				rgba(0, 0, 0, 0)
+			),
+			radial-gradient(
+				2px 2px at 137px 223px,
+				rgb(250, 255, 245),
+				rgb(208, 180, 123),
+				rgba(0, 0, 0, 0)
+			),
+		radial-gradient(
+						2px 2px at 247px 15px,
+						rgb(208, 208, 208),
+						rgb(157, 177, 224),
+						rgba(0, 0, 0, 0)
+		),
+		radial-gradient(
+						2px 2px at 293px 36px,
+						rgb(250, 255, 245),
+						rgb(230, 207, 163),
+						rgba(0, 0, 0, 0)
+		),
+			radial-gradient(1px 2px at 195px 160px, #ddd, rgba(0, 0, 0, 0));
+		background-repeat: repeat;
+		background-size: 300px 300px;
+		opacity: 0;
+		animation: zoom 10s infinite;
+
+		&:nth-child(1) {
+			background-position: 10% 90%;
+			animation-delay: 0s;
+		}
+
+		&:nth-child(2) {
+			background-position: 20% 50%;
+			background-size: 270px 500px;
+			animation-delay: 0.3s;
+		}
+
+		&:nth-child(3) {
+			background-position: 40% -80%;
+			animation-delay: 1.2s;
+		}
+
+		&:nth-child(4) {
+			background-position: -20% -30%;
+			transform: rotate(60deg);
+			animation-delay: 2.5s;
+		}
+
+		&:nth-child(5) {
+			background-image:
+					radial-gradient(2px 2px at 10px 100px, #eee, rgba(0, 0, 0, 0)),
+					radial-gradient(2px 2px at 20px 10px, #fff, rgba(0, 0, 0, 0)),
+					radial-gradient(3px 4px at 150px 40px, #ddd, rgba(0, 0, 0, 0));
+			background-position: 80% 30%;
+			animation-delay: 4s;
+		}
+
+		&:nth-child(6) {
+			background-position: 50% 20%;
+			animation-delay: 6s;
+		}
+	}
+
+	@keyframes zoom {
+		0% {
+			opacity: 0;
+			transform: scale(0.5);
+			transform: rotate(5deg);
+			animation-timing-function: ease-in;
+		}
+		50% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+			transform: scale(2.2);
+		}
+	}
+
+	@media (prefers-reduced-motion) {
+		.stars {
+			animation: none;
+		}
+	}
+</style>
