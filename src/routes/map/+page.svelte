7eb80a0441559818d208ed5a09852ab6a0ce8388
Collin Sinclair <collinsinclair88@gmail.com>
2023-03-14 16:10:28 -0600

    add data to map page

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,12 +1,15 @@
 <script>
 	import { Map, Marker, controls } from '@beyonk/svelte-mapbox';
 	import { onMount } from 'svelte';
+	import coSummitClimbs from '$lib/json/colorado-summit-hikes.json';
 
+	console.log(coSummitClimbs);
 	let mapComponent;
 	const { GeolocateControl, NavigationControl, ScaleControl } = controls;
 	const accessToken =
 		'pk.eyJ1IjoiY29sbGluc2luY2xhaXIiLCJhIjoiY2w4dnl3b2U4MGlvbTNvc3Jna3I0cml4byJ9.GXhvyfjGlngMo7MpNEkMRA';
-	const style = "mapbox://styles/mapbox/outdoors-v12"
+	const style = 'mapbox://styles/mapbox/outdoors-v12';
+	const colors = ['green', 'blue'];
 	onMount(() => {
 		mapComponent.setCenter([-104.99028, 39.73925], 10);
 	});
@@ -20,7 +23,14 @@
 	<Map {accessToken} bind:this={mapComponent} {style}>
 		<GeolocateControl />
 		<NavigationControl />
-		<Marker lat="40" lng="-105" label="Test"/>
+		{#each coSummitClimbs as climb}
+			<Marker
+				lat={climb['Latitude']}
+				lng={climb['Longitude']}
+				label={climb.Name}
+				color={colors[climb['Difficulty']]}
+			/>
+		{/each}
 	</Map>
 </div>
 
